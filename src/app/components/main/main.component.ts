1bc9cc36f3fc65eed9cf1639fc1048c54281e619
McWeb-Lab <luis.web.x@gmail.com>
2025-03-03 21:41:34 +0100

    creacion de toolbar

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ToolbarComponent } from "../toolbar/toolbar.component";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
-  imports: [RouterModule]
+  imports: [RouterModule, ToolbarComponent]
 })
 export class MainComponent implements OnInit {
 
