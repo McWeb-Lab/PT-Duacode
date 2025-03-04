21b622d8b864c92c52f001d5413561375255912a
McWeb-Lab <luis.web.x@gmail.com>
2025-03-04 13:06:21 +0100

    corrección sercicio usuarios

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { UserData } from '../models/user-model';
 
 @Injectable({
@@ -8,8 +8,6 @@ import { UserData } from '../models/user-model';
 })
 export class UserService {
   private apiUrl = 'https://reqres.in/api/users';
-  private usersSubject = new BehaviorSubject<any[]>([]);
-  users$ = this.usersSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
